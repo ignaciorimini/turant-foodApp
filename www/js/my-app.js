e83a8c9776acced38faf61e01ad4bf18f89d5714
Ignacio Lurati <ignaciolurati2@gmail.com>
2021-11-18 23:06:02 -0300

    Navbar 1.2, regitros y login 1.1

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -18,6 +18,7 @@ var app = new Framework7({
     { path: "/about/", url: "about.html" },
     { path: "/registro/", url: "registro.html" },
     { path: "/registrolocal/", url: "registrolocal.html" },
+    { path: "/login/", url: "login.html" },
   ],
   // ... other parameters
 });
@@ -36,6 +37,7 @@ $$(document).on("page:init", function (e) {
   $$("#colapso").removeClass("notoy");
   $$("#colapso").removeClass("toy");
   $$("#colapso").addClass("inicial");
+
   segurocolapso = 0;
 });
 
@@ -48,47 +50,116 @@ $$(document).on("page:init", '.page[data-name="about"]', function (e) {
 $$(document).on("page:init", '.page[data-name="index"]', function (e) {
   // Do something here when page with data-name="about" attribute loaded and initialized
   console.log(e);
-  $$("#lButton").on("click", fnLogin);
 });
 
-$$(document).on("page:init", '.page[data-name="registro"]', function (e) {
+$$(document).on("page:init", '.page[data-name="login"]', function (e) {
   // Do something here when page with data-name="about" attribute loaded and initialized
   console.log(e);
-  $$("#rButton").on("click", fnRegistro);
+  $$("#lButton").on("click", fnLogin);
 
-  $$("#titulito").removeClass("inicial");
-  $$("#titulito").addClass("animate__animated");
-  $$("#titulito").addClass("animate__fadeInUpBig");
+  fallo = 3;
+  guardador = "";
+  console.log(e);
+
+  $$("#lEmail").on("click", function () {
+    if (fallo == 1) {
+      $$("#lEmail").val(guardador);
+    }
+  });
+
+  var random = Math.floor(Math.random() * 5);
 
   setTimeout(function () {
-    $$("#nombre").removeClass("inicial");
-    $$("#nombre").addClass("animate__animated");
-    $$("#nombre").addClass("animate__fadeInLeftBig");
-  }, 500);
+    $$("#titulito" + random).removeClass("inicial");
+    $$("#titulito" + random).addClass("animate__animated");
+    $$("#titulito" + random).addClass("animate__fadeInUpBig");
+  }, 200);
 
   setTimeout(function () {
     $$("#email").removeClass("inicial");
     $$("#email").addClass("animate__animated");
     $$("#email").addClass("animate__fadeInLeftBig");
-  }, 600);
+  }, 700);
 
   setTimeout(function () {
     $$("#contraseña").removeClass("inicial");
     $$("#contraseña").addClass("animate__animated");
     $$("#contraseña").addClass("animate__fadeInLeftBig");
-  }, 700);
+  }, 800);
 
   setTimeout(function () {
     $$("#boton").removeClass("inicial");
     $$("#boton").addClass("animate__animated");
     $$("#boton").addClass("animate__fadeInUp");
-  }, 1500);
+  }, 1700);
 
   setTimeout(function () {
     $$("#boton2").removeClass("inicial");
     $$("#boton2").addClass("animate__animated");
     $$("#boton2").addClass("animate__fadeInUp");
-  }, 1600);
+  }, 1800);
+
+  $$("#colapso").addClass("toy");
+  $$("#colapso").addClass("toy");
+});
+
+$$(document).on("page:init", '.page[data-name="registro"]', function (e) {
+  // Do something here when page with data-name="about" attribute loaded and initialized
+  fallo = 3;
+  guardador = "";
+  console.log(e);
+
+  $$("#rButton").on("click", fnRegistro);
+
+  $$("#rEmail").on("click", function () {
+    if (fallo == 1) {
+      $$("#rEmail").val(guardador);
+    }
+  });
+
+  $$("#rName").on("click", function () {
+    if (fallo == 0) {
+      $$("#rName").val(guardador);
+    }
+  });
+
+  var random = Math.floor(Math.random() * 5);
+
+  setTimeout(function () {
+    $$("#titulito" + random).removeClass("inicial");
+    $$("#titulito" + random).addClass("animate__animated");
+    $$("#titulito" + random).addClass("animate__fadeInUpBig");
+  }, 200);
+
+  setTimeout(function () {
+    $$("#nombre").removeClass("inicial");
+    $$("#nombre").addClass("animate__animated");
+    $$("#nombre").addClass("animate__fadeInLeftBig");
+  }, 700);
+
+  setTimeout(function () {
+    $$("#email").removeClass("inicial");
+    $$("#email").addClass("animate__animated");
+    $$("#email").addClass("animate__fadeInLeftBig");
+  }, 800);
+
+  setTimeout(function () {
+    $$("#contraseña").removeClass("inicial");
+    $$("#contraseña").addClass("animate__animated");
+    $$("#contraseña").addClass("animate__fadeInLeftBig");
+  }, 900);
+
+  setTimeout(function () {
+    $$("#boton").removeClass("inicial");
+    $$("#boton").addClass("animate__animated");
+    $$("#boton").addClass("animate__fadeInUp");
+  }, 1700);
+
+  setTimeout(function () {
+    $$("#boton2").removeClass("inicial");
+    $$("#boton2").addClass("animate__animated");
+    $$("#boton2").addClass("animate__fadeInUp");
+  }, 1800);
 
   $$("#colapso").addClass("toy");
   $$("#colapso").addClass("toy");
@@ -102,16 +173,22 @@ $$(document).on("page:init", '.page[data-name="registrolocal"]', function (e) {
 
 $$("#rayitas").on("click", fnCambio);
 
+var guardador;
 var redirigir = 1;
 var segurocolapso = 0;
 var nombreCliente;
 var db = firebase.firestore();
 var cUsuarios = db.collection("Usuarios");
 var seguroInicio = 0;
+var fallo = 3;
 
 function fnLogin() {
   var emailDelUser = $$("#lEmail").val();
   var passDelUser = $$("#lPass").val();
+  $$("#entradaemail").removeClass("rojo");
+  $$("#lEmail").removeClass("rojazo");
+  $$("#lPass").removeClass("rojazo");
+  $$("#entradacontraseña").removeClass("rojo");
 
   firebase
     .auth()
@@ -122,10 +199,35 @@ function fnLogin() {
       console.log("Bienvenid@!!! " + emailDelUser);
 
       var idUsuarios = emailDelUser;
-
       var docRef = cUsuarios.doc(idUsuarios);
+
       nombreCliente = idUsuarios;
       seguroInicio = 1;
+      $$("#logueo").text(nombreCliente);
+
+      setTimeout(function () {
+        $$(".trigger").toggleClass("drawn");
+      }, 700);
+
+      setTimeout(function () {
+        $$("#ticardo").removeClass("inicial");
+        $$("#tiquito").removeClass("inicial");
+        $$("#desaparezco").addClass("inicial");
+        $$("#desaparezco").removeClass("inicialpro");
+      }, 500);
+
+      setTimeout(function () {
+        redirigir = 1;
+        if (redirigir == 1) {
+          let targetURL = "/index/";
+          let newURL = document.createElement("a");
+          newURL.href = targetURL;
+          document.body.appendChild(newURL);
+          newURL.click();
+        }
+      }, 2500);
+
+      $$("#desaparezco").addClass("inicialpro");
 
       docRef
         .get()
@@ -150,6 +252,28 @@ function fnLogin() {
 
       console.error(errorCode);
       console.error(errorMessage);
+
+      if (errorCode == "auth/email-already-in-use") {
+        $$("#entradaemail").addClass("rojo");
+        $$("#lEmail").addClass("rojazo");
+        guardador = $$("#lEmail").val();
+        $$("#lEmail").val("Email ya en uso");
+        fallo = 1;
+      }
+
+      if (errorCode == "auth/invalid-email") {
+        $$("#entradaemail").addClass("rojo");
+        $$("#lEmail").addClass("rojazo");
+        console.log($$("#lEmail").val() + "hola");
+        guardador = $$("#lEmail").val();
+        $$("#lEmail").val("Email inválido");
+        fallo = 1;
+      }
+
+      if (errorCode == "auth/wrong-password") {
+        $$("#entradacontraseña").addClass("rojo");
+        $$("#lPass").addClass("rojazo");
+      }
     });
 }
 
@@ -165,76 +289,101 @@ function fnRegistro() {
   $$("#rName").removeClass("rojazo");
   $$("#entradanombre").removeClass("rojo");
 
-  if (nombreUser.length <= 2) {
-    $$("#rName").addClass("rojazo");
-    $$("#entradanombre").addClass("rojo");
+  if (emailDelUser.length >= 27) {
+    $$("#rEmail").addClass("rojazo");
+    $$("#entradaemail").addClass("rojo");
+    guardador = $$("#rEmail").val();
+    $$("#rEmail").val("Máximo 26 caracteres");
+    fallo = 1;
   } else {
-    firebase
-      .auth()
-      .createUserWithEmailAndPassword(emailDelUser, passDelUser)
-      .then((userCredential) => {
-        // Signed in
-        var user = userCredential.user;
-        console.log("Bienvenid@!!! " + emailDelUser);
-        // ...
-        // mainView.router.navigate('/siguientePantallaDeUsuarioOK/');
+    if (nombreUser.length <= 2) {
+      $$("#rName").addClass("rojazo");
+      $$("#entradanombre").addClass("rojo");
+      guardador = $$("#rName").val();
+      $$("#rName").val("Mínimo 3 caracteres");
+      fallo = 0;
+    } else if (nombreUser.length >= 27) {
+      $$("#rName").addClass("rojazo");
+      $$("#entradanombre").addClass("rojo");
+      guardador = $$("#rName").val();
+      $$("#rName").val("Máximo 26 caracteres");
+      fallo = 0;
+    } else {
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(emailDelUser, passDelUser)
+        .then((userCredential) => {
+          // Signed in
+          var user = userCredential.user;
+          console.log("Bienvenid@!!! " + emailDelUser);
+          // ...
+          // mainView.router.navigate('/siguientePantallaDeUsuarioOK/');
 
-        var idUsuarios = emailDelUser;
+          var idUsuarios = emailDelUser;
 
-        nombre = $$("#rName").val();
-        setTimeout(function () {
-          $$(".trigger").toggleClass("drawn");
-        }, 700);
+          nombre = $$("#rName").val();
+          setTimeout(function () {
+            $$(".trigger").toggleClass("drawn");
+          }, 700);
 
-        setTimeout(function () {
-          $$("#ticardo").removeClass("inicial");
-          $$("#tiquito").removeClass("inicial");
-          $$("#desaparezco").addClass("inicial");
-          $$("#desaparezco").removeClass("inicialpro");
-        }, 500);
+          setTimeout(function () {
+            $$("#ticardo").removeClass("inicial");
+            $$("#tiquito").removeClass("inicial");
+            $$("#desaparezco").addClass("inicial");
+            $$("#desaparezco").removeClass("inicialpro");
+          }, 500);
 
-        setTimeout(function () {
-          redirigir = 1;
-          if (redirigir == 1) {
-            let targetURL = "/index/";
-            let newURL = document.createElement("a");
-            newURL.href = targetURL;
-            document.body.appendChild(newURL);
-            newURL.click();
+          setTimeout(function () {
+            redirigir = 1;
+            if (redirigir == 1) {
+              let targetURL = "/index/";
+              let newURL = document.createElement("a");
+              newURL.href = targetURL;
+              document.body.appendChild(newURL);
+              newURL.click();
+            }
+          }, 2500);
+
+          $$("#desaparezco").addClass("inicialpro");
+
+          datos = {
+            nombre: nombre,
+            rol: "usuario",
+          };
+
+          cUsuarios.doc(idUsuarios).set(datos);
+          nombreCliente = idUsuarios;
+          $$("#logueo").text(nombreCliente);
+        })
+        .catch((error) => {
+          var errorCode = error.code;
+          var errorMessage = error.message;
+
+          console.error(errorCode);
+          console.error(errorMessage);
+
+          if (errorCode == "auth/email-already-in-use") {
+            $$("#entradaemail").addClass("rojo");
+            $$("#rEmail").addClass("rojazo");
+            guardador = $$("#rEmail").val();
+            $$("#rEmail").val("Email ya en uso");
+            fallo = 1;
           }
-        }, 2500);
 
-        $$("#desaparezco").addClass("inicialpro");
+          if (errorCode == "auth/invalid-email") {
+            $$("#entradaemail").addClass("rojo");
+            $$("#rEmail").addClass("rojazo");
+            guardador = $$("#rEmail").val();
+            $$("#rEmail").val("Email inválido");
+            fallo = 1;
+          }
 
-        datos = {
-          nombre: nombre,
-          rol: "usuario",
-        };
-
-        cUsuarios.doc(idUsuarios).set(datos);
-      })
-      .catch((error) => {
-        var errorCode = error.code;
-        var errorMessage = error.message;
-
-        console.error(errorCode);
-        console.error(errorMessage);
-
-        if (errorCode == "auth/email-already-in-use") {
-          $$("#entradaemail").addClass("rojo");
-          $$("#rEmail").addClass("rojazo");
-        }
-
-        if (errorCode == "auth/invalid-email") {
-          $$("#entradaemail").addClass("rojo");
-          $$("#rEmail").addClass("rojazo");
-        }
-
-        if (errorCode == "auth/weak-password") {
-          $$("#entradacontraseña").addClass("rojo");
-          $$("#rPass").addClass("rojazo");
-        }
-      });
+          if (errorCode == "auth/weak-password") {
+            $$("#entradacontraseña").addClass("rojo");
+            $$("#rPass").addClass("rojazo");
+          }
+        });
+    }
   }
 }
 
